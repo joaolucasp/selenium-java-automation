c5b943686c5c4818f9aece961b7d0b53b3c30e7a
João Lucas Pereira <j.lucaspereira0@gmail.com>
2026-02-19 17:35:25 -0300

    section 29: selenium-grid

---
+++ selenium-grid/src/main/java/training/GoogleTest.java
@@ -0,0 +1,29 @@
+package training;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class GoogleTest {
+
+  @Test
+  public void HomePageClick() throws MalformedURLException, URISyntaxException {
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setBrowserName("chrome");
+    //caps.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+    //caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+
+    WebDriver driver = new RemoteWebDriver(new URI("http://192.168.1.21:4444").toURL(), caps);
+    driver.get("https://google.com");
+    System.out.println(driver.getTitle());
+    driver.findElement(By.name("q")).sendKeys("Linkedin");
+    driver.close();
+  }
+}
